d14ff6994b053f07ce770e987d19f3d3706188b9
Andrii Ponomarov <andrii.ponomarov@gmail.com>
2016-06-11 00:19:01 -0400

    Add a Hint code example for unknown errors

--- spec/budd/hint_spec.rb
+++ spec/budd/hint_spec.rb
@@ -6,4 +6,11 @@ describe Budd::Hint do
     hint = Budd::Hint.for(exception)
     _(hint).must_be_instance_of Budd::Hints::ArgumentError
   end
+
+  it 'returns nil for unknown exception' do
+    UnknownError = Class.new
+    exception = UnknownError.new
+    hint = Budd::Hint.for(exception)
+    _(hint).must_be_nil
+  end
 end
